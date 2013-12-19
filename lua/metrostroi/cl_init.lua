b87fc9608b2d9b43288576d55bd1bf5fd8c4b7a0
HunterNL <hunternl1@gmail.com>
2013-12-19 01:58:40 +0100

    Improved aim location detection

--- lua/metrostroi/cl_init.lua
+++ lua/metrostroi/cl_init.lua
@@ -14,6 +14,23 @@ local function WorldToScreen(vWorldPos,vPos,vScale,aRot)
     return vWorldPos.x/vScale,(-vWorldPos.y)/vScale;
 end
 
+//There's got to be a better way to do this all
+local function copyVec(v)
+	return Vector(v[1],v[2],v[3])
+end
+
+//I seriously ragequit developing this function, not because of the math but because vector functions modify the original,
+//wich fucks up references
+local function LinePlaneIntersect(PlanePos,PlaneNormal,LinePos,LineDir)
+	local dot = LineDir:Dot(PlaneNormal)
+	local fac = copyVec(LinePos) //Stupid Sub modifying the origional
+	fac:Sub(PlanePos)
+	local dis = -PlaneNormal:Dot(fac) / dot
+	local LdCopy = copyVec(LineDir)
+	LdCopy:Mul(dis)
+	LinePos:Add(LdCopy)
+	return LinePos
+end
 
 hook.Add("KeyPress", "Metrostroi_Cabin_Buttons", function(ply, key)
 	if not(key==IN_ATTACK or key==IN_ATTACK2) then return end 
@@ -26,20 +43,23 @@ hook.Add("KeyPress", "Metrostroi_Cabin_Buttons", function(ply, key)
 	//if seat != train.DriverSeat then return end
 	if train.ButtonMap == nil then return end
 	
-	local tr = ply:GetEyeTrace()
-	local aimPos 
-  
-	if tr.Hit then 
-		aimPos = tr.HitPos
-		debugoverlay.Cross(aimPos,2,10,Color(255,255,255),true)
-	else
-		return 
-	end
+	local tr = ply:GetEyeTrace()  
+	if !tr.Hit then return end
 	
 	
 	for kp,panel in pairs(train.ButtonMap) do
+		local wpos = train:LocalToWorld(panel.pos)
+		local wang = train:LocalToWorldAngles(panel.ang)
+		
+		//See note at definition
+		local v1 = copyVec(wpos)
+		local v2 = copyVec(wang:Up())
+		local v3 = copyVec(tr.StartPos)
+		local v4 = copyVec(tr.Normal)
+		local isectPos = LinePlaneIntersect(v1,v2,v3,v4)
+		local localx,localy = WorldToScreen(isectPos,wpos,panel.scale,wang)
+		debugoverlay.Cross(isectPos,2,10,Color(255,255,255),true) //Only shows up when developer = 1
 		for kb,button in pairs(panel.buttons) do
-			local localx,localy = WorldToScreen(aimPos,train:LocalToWorld(panel.pos),panel.scale,train:LocalToWorldAngles(panel.ang))
 			if math.Dist(button[1],button[2],localx,localy) < 10 then
 				net.Start("metrostroi-cabin-button")
 				net.WriteInt(kp,8)
