19c1a7980a3376c85c79baec771a53dd0316f3a9
HunterNL <hunternl1@gmail.com>
2014-01-26 15:49:17 +0100

    Gamemode init

---
+++ gamemodes/metrostroi/gamemode/shared.lua
@@ -0,0 +1,9 @@
+GM.Name = "Metrostroi"
+GM.Author = "HunterNL, Black Phoenix"
+GM.Email = "N/A"
+GM.Website = "N/A"
+
+function GM:Initialize()
+	self.BaseClass.Initialize( self )
+	print("metroistroi gm init")
+end 
