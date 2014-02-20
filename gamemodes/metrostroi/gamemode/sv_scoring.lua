0ab05f8a6c8c770be98203a76c174d74257e4a86
Daniel <daniel.cf.hultgren@gmail.com>
2014-02-20 19:06:03 +0100

    Added reason and stationid column to scoring

--- gamemodes/metrostroi/gamemode/sv_scoring.lua
+++ gamemodes/metrostroi/gamemode/sv_scoring.lua
@@ -1,4 +1,8 @@
 
+MetroScoreReasons = {}
+MetroScoreReasons.Other = 0
+MetroScoreReasons.MissedSchedule = 1
+MetroScoreReasons.PeakHourBonus = 2
 hook.Add("Initialize", "SetupSQL_Scoring", function()
 	--SQLQuery("DROP TABLE IF EXISTS `metroscoring`")
 	
@@ -6,15 +10,19 @@ hook.Add("Initialize", "SetupSQL_Scoring", function()
 	CREATE TABLE IF NOT EXISTS `metroscoring` (
 		`steamid` varchar(19) NOT NULL,
 		`score` int(32) NOT NULL,
-		`timestamp` int(32) NOT NULL
+		`timestamp` int(32) NOT NULL,
+		`reason` int(8) NOT NULL DEFAULT '0',
+		`stationid` int(8) NOT NULL DEFAULT '0'
 	)]])
 end)
 
 local plymeta = FindMetaTable("Player")
-function plymeta:AddMetroScore( am )
+function plymeta:AddMetroScore( am, reason, stationid )
 	am = math.Clamp(am, -2147483648, 2147483647) -- Clamp it to a 32bit, is this needed?
+	reason = reason or MetroScoreReasons.Other
+	stationid = stationid or 0
 	
-	SQLQuery("INSERT INTO `metroscoring` VALUES('%s', '%i', '%i');", self:SteamID(), am, os.time())
+	SQLQuery("INSERT INTO `metroscoring` VALUES('%s', '%i', '%i', '%i', '%i');", self:SteamID(), am, os.time(), reason, stationid)
 end
 
 function plymeta:GetTotalMetroScore()
